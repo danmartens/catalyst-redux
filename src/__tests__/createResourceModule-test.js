5ee066d8e1542f81d3960ab9a22a2e05448b1250
Dan Martens <dan@friendsoftheweb.com>
2017-11-24 20:40:52 -0500

    Find operation tests

--- src/__tests__/createResourceModule-test.js
+++ src/__tests__/createResourceModule-test.js
@@ -167,6 +167,58 @@ test('find returns an object with relationships', () => {
   expect(secondComment.post).toBe(firstPost);
 });
 
+test('find operation', async () => {
+  const store = createResourceStore();
+  let state = store.getState();
+
+  axios.__setNextResponse(
+    'GET',
+    {
+      data: {
+        type: 'posts',
+        id: '3',
+        attributes: { title: 'Something New' }
+      }
+    },
+    200
+  );
+
+  await nextState(store, Resources.actions.find('posts', 3));
+
+  state = store.getState();
+
+  expect(Resources.selectors.resourceStatus(state, 'posts', 3)).toEqual(
+    'find.pending'
+  );
+
+  await nextState(store);
+
+  expect(Resources.selectors.find(store.getState(), 'posts', 3)).toEqual({
+    id: '3',
+    title: 'Something New'
+  });
+});
+
+test('find operation failed', async () => {
+  const store = createResourceStore();
+
+  axios.__setNextResponse('GET', {}, 401);
+
+  await nextState(store, Resources.actions.find('posts', 4));
+
+  expect(
+    Resources.selectors.resourceStatus(store.getState(), 'posts', 4)
+  ).toEqual('find.pending');
+
+  await nextState(store);
+
+  expect(
+    Resources.selectors.resourceStatus(store.getState(), 'posts', 4)
+  ).toEqual('find.error');
+
+  expect(Resources.selectors.findAll(store.getState(), 'posts')).toEqual([]);
+});
+
 test('findAll operation', async () => {
   const store = createResourceStore();
   let state = store.getState();
